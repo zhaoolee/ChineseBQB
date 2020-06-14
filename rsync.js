81736bce0c81bd7c806d428665901e27cde097e4
zhaoolee <zhaoolee@gmail.com>
2020-06-14 15:31:37 +0800

    zhaoolee commit

--- rsync.js
+++ rsync.js
@@ -165,7 +165,7 @@ async function main() {
   // 改权限
 
   await md_and_img_chmodr(path.join(__dirname, md_dir_name));
-  await md_and_img_chmodr(path.join(__dirname))
+  await md_and_img_chmodr(path.join(__dirname, "*BQB"))
   
 
   // 先同步数据到服务端
